89f04edeb6ce7403f58c73d836d97b04daca8295
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2024-09-17 18:22:28 +0530

    Updated the Java Code

--- SEM-1/Core-Java/Piece-of-Codes/Practical-Session-03(10-sept-2024)/WordSorter.java
+++ SEM-1/Core-Java/Piece-of-Codes/Practical-Session-03(10-sept-2024)/WordSorter.java
@@ -7,25 +7,25 @@ public class WordSorter {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
         System.out.println("Enter a list of words separated by spaces:");
         String input = scanner.nextLine();
         scanner.close();
-        
+
         String[] wordsArray = input.split("\\s+");
-        
+
         List<String> wordsList = Arrays.asList(wordsArray);
-        
+
         Collections.sort(wordsList);
-        
+
         System.out.println("Words in alphabetical order:");
         for (String word : wordsList) {
             System.out.print(word + " ");
         }
         System.out.println();
-        
+
         Collections.reverse(wordsList);
-        
+
         System.out.println("Words in reverse alphabetical order:");
         for (String word : wordsList) {
             System.out.print(word + " ");
