c1dc16b40c3afffac4de42d641023df3d6e232bb
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2024-09-19 20:23:39 +0530

    Update Core Java code

--- SEM-1/Core-Java/Piece-of-Codes/Practical-Session-02(02-sept-2024)/ShapeMainWithScanner.java
+++ SEM-1/Core-Java/Piece-of-Codes/Practical-Session-02(02-sept-2024)/ShapeMainWithScanner.java
@@ -2,23 +2,24 @@ import java.util.Scanner;
 
 // Base Abstract class
 abstract class Shape {
-    public abstract double surfaceArea();
-    public abstract double volume();
+	public abstract double surfaceArea();
+
+	public abstract double volume();
 }
 
 // Derived class for Sphere
 class Sphere extends Shape {
 	private double radius;
-	
+
 	public Sphere(double radius) {
 		this.radius = radius;
 	}
-	
+
 	@Override
 	public double surfaceArea() {
 		return 4 * Math.PI * Math.pow(radius, 2);
 	}
-	
+
 	@Override
 	public double volume() {
 		return (4.0 / 3) * Math.PI * Math.pow(radius, 3);
@@ -28,16 +29,16 @@ class Sphere extends Shape {
 // Derived class for Cube
 class Cube extends Shape {
 	private double side;
-	
+
 	public Cube(double side) {
 		this.side = side;
 	}
-	
+
 	@Override
 	public double surfaceArea() {
 		return 6 * Math.pow(side, 2);
 	}
-	
+
 	@Override
 	public double volume() {
 		return Math.pow(side, 3);
@@ -49,18 +50,18 @@ class RectangularPrism extends Shape {
 	private double length;
 	private double width;
 	private double height;
-	
+
 	public RectangularPrism(double length, double width, double height) {
 		this.length = length;
 		this.width = width;
 		this.height = height;
 	}
-	
+
 	@Override
 	public double surfaceArea() {
 		return 2 * (length * width + width * height + height * length);
 	}
-	
+
 	@Override
 	public double volume() {
 		return length * width * height;
@@ -71,17 +72,17 @@ class RectangularPrism extends Shape {
 class RightCircularCylinder extends Shape {
 	private double radius;
 	private double height;
-	
+
 	public RightCircularCylinder(double radius, double height) {
 		this.radius = radius;
 		this.height = height;
 	}
-	
+
 	@Override
 	public double surfaceArea() {
 		return 2 * Math.PI * radius * (radius + height);
 	}
-	
+
 	@Override
 	public double volume() {
 		return Math.PI * Math.pow(radius, 2) * height;
@@ -91,7 +92,7 @@ class RightCircularCylinder extends Shape {
 // Main class
 public class ShapeMainWithScanner {
 	public static void main(String[] args) {
-		
+
 		// Sphere With User input
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter the radius value for Sphere:");
@@ -100,7 +101,7 @@ public class ShapeMainWithScanner {
 		System.out.println("Sphere:");
 		System.out.println("Surface Area: " + sphere.surfaceArea());
 		System.out.println("Volume: " + sphere.volume());
-		
+
 		// Cube With User Input
 		System.out.println("\nEnter the side value for Cube:");
 		double side = scan.nextDouble();
@@ -108,7 +109,7 @@ public class ShapeMainWithScanner {
 		System.out.println("\nCube:");
 		System.out.println("Surface Area: " + cube.surfaceArea());
 		System.out.println("Volume: " + cube.volume());
-		
+
 		// Rectangular Prism With User Input
 		System.out.println("\nEnter the length value for Rectangular Prism:");
 		double length = scan.nextDouble();
@@ -120,7 +121,7 @@ public class ShapeMainWithScanner {
 		System.out.println("\nRectangular Prism:");
 		System.out.println("Surface Area: " + rectangularPrism.surfaceArea());
 		System.out.println("Volume: " + rectangularPrism.volume());
-		
+
 		// Right Circular Cylinder With User Input
 		System.out.println("\nEnter the radius value for Right Circular Cylinder:");
 		double radius2 = scan.nextDouble();
@@ -130,5 +131,7 @@ public class ShapeMainWithScanner {
 		System.out.println("\nRight Circular Cylinder:");
 		System.out.println("Surface Area: " + cylinder.surfaceArea());
 		System.out.println("Volume: " + cylinder.volume());
+
+		scan.close();
 	}
 }
