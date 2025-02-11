6bab63ce51cbba9a3c9ec7f8ba386b65cb8e3bf4
Mohammed <95087498+Mohammedvaraliya@users.noreply.github.com>
2025-02-11 23:22:12 +0530

    Added Mobile Application Development "2nd Session Code"

--- SEM-2/Mobile-Application-Development/Session2MultiActivityApp/app/src/main/java/com/example/session2multiactivityapp/MainActivity2.java
+++ SEM-2/Mobile-Application-Development/Session2MultiActivityApp/app/src/main/java/com/example/session2multiactivityapp/MainActivity2.java
@@ -1,6 +1,12 @@
 package com.example.session2multiactivityapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +15,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    Button backButton, search;
+    TextView query, query2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +29,43 @@ public class MainActivity2 extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        backButton = findViewById(R.id.button_back);
+
+        Intent intent = getIntent();
+        int userId = intent.getIntExtra("userId", 0);
+        String email = intent.getStringExtra("email");
+
+        Toast.makeText(this, "User Id: " + userId + "\nEmail: " + email , Toast.LENGTH_LONG).show();
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        query = findViewById(R.id.query);
+        query2 = findViewById(R.id.query2);
+        search = findViewById(R.id.search);
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent googleSearch = new Intent(Intent.ACTION_VIEW);
+                if (!query.getText().toString().isEmpty()) {
+                    googleSearch.setData(Uri.parse("https://www.google.com/search?q=" + query.getText().toString()));
+                } else if (!query2.getText().toString().isEmpty()) {
+                    googleSearch.setData(Uri.parse("geo:0,0?q=" + query2.getText().toString()));
+                } else {
+                    Toast.makeText(MainActivity2.this, "Please enter a search query or location.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                startActivity(googleSearch);
+                finish();
+            }
         });
     }
 }
